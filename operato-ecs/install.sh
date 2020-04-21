52ad821b10b65338959c2e93c56242ecc75b3f35
jyp220 <jyp@hatiolab.com>
2020-04-21 14:31:23 +0900

    install.sh update

--- operato-ecs/install.sh
+++ operato-ecs/install.sh
@@ -5,11 +5,25 @@ else
   curl -O https://raw.githubusercontent.com/things-factory/installer/master/operato-ecs/db.sqlite
 fi
 
-curl -O https://raw.githubusercontent.com/things-factory/installer/master/operato-ecs/docker-compose.yml
-
 if [ -f "config.production.js" ] ; then
   echo "config.production.js exist"
 else
   echo "config.production.js create"
   curl -O https://raw.githubusercontent.com/things-factory/installer/master/operato-ecs/config.production.js
 fi
+
+if [ -f "operato-ecs-start.sh" ] ; then
+  echo "operato-ecs-start.sh exist"
+else
+  echo "operato-ecs-start.sh create"
+  curl -O https://raw.githubusercontent.com/things-factory/installer/master/operato-ecs/operato-ecs-start.sh
+fi
+
+if [ -f "operato-ecs-stop.sh" ] ; then
+  echo "operato-ecs-stop.sh exist"
+else
+  echo "operato-ecs-stop.sh create"
+  curl -O https://raw.githubusercontent.com/things-factory/installer/master/operato-ecs/operato-ecs-stop.sh
+fi
+
+curl -O https://raw.githubusercontent.com/things-factory/installer/master/operato-ecs/docker-compose.yml
