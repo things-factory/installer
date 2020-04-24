995f8d2516a422520443d6de5cd75f13c659f477
Hearty Oh <heartyoh@hatiolab.com>
2020-04-24 17:36:55 +0900

    Update emulator-mqtt-sensor.sh

--- operato-ecs-tutorial/emulator-mqtt-sensor.sh
+++ operato-ecs-tutorial/emulator-mqtt-sensor.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-
 if [ -z "$1" ]; then 
   TAG='1'
 else 
   TAG=$1
+fi
+
+if [ -z "$2" ]; then 
+  HOST='localhost'
+else 
+  HOST=$2
 fi
 
 while :
@@ -13,6 +17,6 @@ do
 
   JSON_STRING='{"tag":"'"$TAG"'","value":"'"$VALUE"'","timestamp":"'"$TIMESTAMP"'"}'
   echo $JSON_STRING
-  mosquitto_pub -t test -m $JSON_STRING
+  mosquitto_pub -h $HOST -t test -m $JSON_STRING
   sleep 1
 done
