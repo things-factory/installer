d3e619df3a98ae958916f0c2c985fc928f9ae7bb
Hearty, Oh <heartyoh@hatiolab.com>
2020-05-07 11:19:22 +0900

    Create config.production.arm.js

---
+++ operato-ecs/config.production.arm.js
@@ -0,0 +1,11 @@
+module.exports = {
+  CHROMIUM_PATH: "/usr/bin/chromium-browser",
+  SECRET: "0xD58F835B69D207A76CC5F84a70a1D0d4C79dAC95",
+  ormconfig: {
+    name: "default",
+    type: "sqlite",
+    database: "db.sqlite",
+    synchronize: true,
+    logging: true,
+  },
+};
